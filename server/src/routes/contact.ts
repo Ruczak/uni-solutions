a8204af925e937d3949436d080de3918eba9aed5
Ruczak <igor.sarnowski@gmail.com>
2022-02-09 13:43:12 +0100

    Added phone option in contact request

--- server/src/routes/contact.ts
+++ server/src/routes/contact.ts
@@ -12,16 +12,32 @@ router.post('/contact', checkForm, async (req: Request, res: Response) => {
 
     const body = Object.assign({ sent: new Date() }, req.body);
 
+    stmt.input('firstname', sql.VarChar(40));
+    stmt.input('lastname', sql.VarChar(40));
+    stmt.input('email', sql.VarChar(80));
+
+    stmt.input('subject', sql.VarChar(40));
+    stmt.input('description', sql.VarChar);
+    stmt.input('sent', sql.DateTime);
+
     if (body.phone) {
+      stmt.input('phone', sql.Char(14));
+
       const query =
         'INSERT INTO ContactRequests(FirstName, LastName, Email, Phone, Subject, Description, SendTime) VALUES (@firstname, @lastname, @email, @phone, @subject, @description, @sent)';
-      stmt.input('firstname', sql.VarChar(40));
-      stmt.input('lastname', sql.VarChar(40));
-      stmt.input('email', sql.VarChar(80));
-      stmt.input('phone', sql.Char(14));
-      stmt.input('subject', sql.VarChar(40));
-      stmt.input('description', sql.VarChar);
-      stmt.input('sent', sql.DateTime);
+
+      stmt.prepare(query, (err) => {
+        if (err) throw err;
+        stmt.execute(body, (err, result) => {
+          if (err) throw err;
+
+          res.status(201).send({ addedRows: result?.rowsAffected[0] });
+        });
+      });
+    } else {
+      const query =
+        'INSERT INTO ContactRequests(FirstName, LastName, Email, Subject, Description, SendTime) VALUES (@firstname, @lastname, @email, @subject, @description, @sent)';
+
       stmt.prepare(query, (err) => {
         if (err) throw err;
         stmt.execute(body, (err, result) => {

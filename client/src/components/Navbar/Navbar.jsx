6468884348ba8d3da3dd6801a30db22f4f3394c1
Ruczak <igor.sarnowski@gmail.com>
2022-03-08 10:12:46 +0100

    Created link visibility toggler

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from './Link';
 import Logo from '../Logo';
+import Hamburger from './Hamburger';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen((isOpen) => !isOpen);
+  };
+
+  // useEffect(() => console.log(isOpen), [isOpen]);
+
   return (
     <div className="navbar">
-      <Logo />
-      <Link>About us</Link>
-      <Link>Projects</Link>
-      <Link>Blog</Link>
-      <Link>Contact</Link>
+      <Logo size={64} />
+      <Hamburger size="xl" onClick={handleClick} />
+      <div className="navbar__break"></div>
+      <div
+        className="navbar__menu"
+        style={{ display: isOpen ? 'flex' : 'none' }}
+      >
+        <Link>About us</Link>
+        <Link>Projects</Link>
+        <Link>Blog</Link>
+        <Link>Contact</Link>
+      </div>
     </div>
   );
 };

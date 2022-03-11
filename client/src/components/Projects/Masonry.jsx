1b2665f5e5a568bb80025aadb3f7c3a73d125e15
Ruczak <igor.sarnowski@gmail.com>
2022-03-11 16:34:35 +0100

    Created Masonry component for masonry layout

---
+++ client/src/components/Projects/Masonry.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
+const Masonry = ({ cols, gap, children }) => {
+  return (
+    <div className="masonry">
+      {Array.from(Array(cols).keys()).map((col) => {
+        return (
+          <div className="masonry__col" style={{ marginRight: gap }} key={col}>
+            {React.Children.toArray(children)
+              .filter((child, i) => i % 3 === col)
+              .map((child, i) => {
+                return (
+                  <div
+                    className="masonry__element"
+                    style={{ marginBottom: gap }}
+                    key={i}
+                  >
+                    {child}
+                  </div>
+                );
+              })}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Masonry;

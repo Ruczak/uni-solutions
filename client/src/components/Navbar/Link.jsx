7471002c22d2f8a7aa633f7366370af8c5b51940
Ruczak <igor.sarnowski@gmail.com>
2022-03-14 17:49:57 +0100

    Changed styling of Link component

--- client/src/components/Navbar/Link.jsx
+++ client/src/components/Navbar/Link.jsx
@@ -4,7 +4,15 @@ import { Link as RouterLink } from 'react-router-dom';
 const Link = ({ children, href }) => {
   return (
     <div className="link">
-      <RouterLink style={{ textDecoration: 'none', color: 'black' }} to={href}>
+      <RouterLink
+        style={{
+          textDecoration: 'none',
+          color: 'black',
+          width: '100%',
+          display: 'inline-block'
+        }}
+        to={href}
+      >
         {children}
       </RouterLink>
     </div>

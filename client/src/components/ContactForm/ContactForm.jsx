f269f1e85b6896cd21a9639ae42ecc30b9a530b6
Ruczak <igor.sarnowski@gmail.com>
2022-03-17 11:37:29 +0100

    Added Modal view to contact on submit and error

--- client/src/components/ContactForm/ContactForm.jsx
+++ client/src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Modal from '../Modal/Modal';
 import TextInput from './TextInput';
 
 const ContactForm = () => {
@@ -8,6 +9,7 @@ const ContactForm = () => {
   const [subject, setSubject] = useState('');
   const [description, setDescription] = useState('');
   const [error, setError] = useState(null);
+  const [modal, setModal] = useState(false);
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -48,57 +50,81 @@ const ContactForm = () => {
 
       if (!response.ok) throw new Error(body.error);
 
-      window.alert('Successfully added ' + body.addedRows);
+      console.log('ses');
+
+      setError(null);
+      setModal(true);
     } catch (error) {
       setError(error);
     }
   };
 
   useEffect(() => {
-    if (error) window.alert(error.message);
+    if (error) setModal(true);
   }, [error]);
 
   return (
-    <form className="contact" onSubmit={(e) => submitHandler(e)}>
-      <TextInput
-        label="Name"
-        onChange={(e) => setName(e.target.value)}
-        value={name}
-        fill
-      />
-      <TextInput
-        label="Email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-        email={true}
-      />
-      <TextInput
-        label="Phone"
-        onChange={(e) => setPhone(e.target.value)}
-        value={phone}
-      />
-
-      <div className="contact__message">
-        <span className="contact__label">Subject: </span>
-        <input
-          type="text"
-          className="contact__text-input"
-          value={subject}
-          onChange={(e) => setSubject(e.target.value)}
+    <React.Fragment>
+      <form className="contact" onSubmit={(e) => submitHandler(e)}>
+        <h1 className="contact__title">Contact form</h1>
+        <TextInput
+          label="Name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          fill
         />
-        <span className="contact__label">Description: </span>
-        <textarea
-          className="contact__text-area"
-          cols="30"
-          rows="8"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        ></textarea>
-      </div>
-      <button type="submit" className="contact__submit">
-        SEND
-      </button>
-    </form>
+        <TextInput
+          style={{ width: '50%', paddingRight: '5px' }}
+          label="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          email={true}
+        />
+        <TextInput
+          style={{ width: '50%' }}
+          label="Phone"
+          onChange={(e) => setPhone(e.target.value)}
+          value={phone}
+        />
+
+        <div className="contact__message">
+          <span className="contact__label">Subject: </span>
+          <input
+            type="text"
+            className="contact__text-input"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+          />
+          <span className="contact__label">Description: </span>
+          <textarea
+            className="contact__text-area"
+            cols="30"
+            rows="8"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
+        </div>
+        <button type="submit" className="contact__submit">
+          SEND
+        </button>
+      </form>
+      <Modal
+        type={error ? 'failure' : 'success'}
+        title={error ? 'Sending failed' : 'Success!'}
+        description={
+          error ? error.message : 'Successfully sent contact request!'
+        }
+        options={[
+          { label: 'OK', action: () => {} },
+          {
+            label: 'Return to home page',
+            action: () => (window.location.href = '/')
+          }
+        ]}
+        onDisplay={(v) => setModal(v)}
+        show={modal}
+      />
+    </React.Fragment>
   );
 };
 
